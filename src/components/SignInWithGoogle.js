f5c194ecd87ad5960069c4bb12a3c18da82caeda
Kenneth Carter <kxccarter@gmail.com>
2020-12-08 15:50:58 -0600

    updates site description and favicon

--- src/components/SignInWithGoogle.js
+++ src/components/SignInWithGoogle.js
@@ -4,15 +4,11 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-
 // MUI
 import { Button } from '@material-ui/core';
 
 const SignInWithGoogle = (props) => {
   const auth = firebase.auth();
-  const firestore = firebase.firestore();
 
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
